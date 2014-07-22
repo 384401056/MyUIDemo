6570c949b0603d27c0e044fd156941182bbe262f
GaoYanbin <384401056@qq.com>
2014-07-22 23:50:35 +0800

    UI05 finish : add ProgressBar Seekbar

--- ui05_standerView02/src/main/java/com/blueice/ui05/MainActivity.java
+++ ui05_standerView02/src/main/java/com/blueice/ui05/MainActivity.java
@@ -2,11 +2,14 @@ package com.blueice.ui05;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.ProgressBar;
 import android.widget.RadioGroup;
+import android.widget.SeekBar;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
@@ -19,19 +22,29 @@ public class MainActivity extends Activity {
     private Button btn;
     private ToggleButton togBtn;
     private CheckBox cb;
-    private RadioGroup rg;
 
+    private ProgressBar prg01;
+    private SeekBar sbar01;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        prg01 = (ProgressBar)findViewById(R.id.pg01);
+        sbar01 = (SeekBar)findViewById(R.id.sb01);
+
         btn = (Button)findViewById(R.id.btn01);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this,"点击了Button",Toast.LENGTH_SHORT).show();
+
+                prg01.setProgress(prg01.getProgress()+3);
+                if(prg01.getProgress()>=prg01.getMax()){
+                    prg01.setProgress(0);
+                }
+
             }
         });
 
@@ -66,6 +79,40 @@ public class MainActivity extends Activity {
                 else{
 //                    buttonView.setButtonDrawable(R.drawable.select_1);
                 }
+            }
+        });
+
+
+        /* 当thumd拖动时的监听事件 */
+        sbar01.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            /**
+             * 当ProgressBar在拖动过程中执行的方法。
+             * @param seekBar
+             * @param progress
+             * @param fromUser
+             */
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.i("MyLog", "正在拖动.........");
+            }
+
+            /**
+             * 开始点击拖动按钮时执行的方法
+             * @param seekBar
+             */
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Log.i("MyLog", "点击了按钮，要开始拖啦......");
+            }
+
+            /**
+             * 结束拖动时执行的方法。
+             * @param seekBar
+             */
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.i("MyLog", "放开按钮，不拖了......");
             }
         });
 
